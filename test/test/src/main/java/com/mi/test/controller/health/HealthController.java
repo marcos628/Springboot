0a311052d81b4636fab2d743ff30444f0535fff6
marcos628 <123183574+marcos628@users.noreply.github.com>
2024-07-09 16:01:40 -0600

    healthController

---
+++ test/test/src/main/java/com/mi/test/controller/health/HealthController.java
@@ -0,0 +1,16 @@
+package com.mi.test.controller.health;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping
+    public String checkAPI() {
+        return "<h1>The API is working ok!</h1>";
+    }
+
+}
